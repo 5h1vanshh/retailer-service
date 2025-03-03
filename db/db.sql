f034a6e660b1455755279549ddacd40dbe5082e6
5h1vanshh <shivansh.paliwal@razorpay.com>
2025-03-03 11:47:14 +0530

    server running successfully

--- db/db.sql
+++ db/db.sql
@@ -18,4 +18,34 @@ CREATE TABLE orders (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 show databases
-SHOW TABLES;
+SHOW TABLes;
+SELECT * FROM products;
+DESC products;
+ALTER TABLE products MODIFY id INT PRIMARY KEY AUTO_INCREMENT;
+ALTER TABLE products DROP PRIMARY KEY;
+ALTER TABLE products MODIFY id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+
+CREATE TABLE products_new (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name LONGTEXT,
+    price DOUBLE,
+    quantity BIGINT
+);
+INSERT INTO products_new (product_name, price, quantity)
+SELECT product_name, price, quantity FROM products;
+
+
+
+DROP TABLE products;
+ALTER TABLE products_new RENAME TO products;
+
+DESC products;
+ALTER TABLE products MODIFY id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+
+ALTER TABLE products MODIFY id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+ SHOW CREATE TABLE products;
+
+
+
+
+
