9b79894d2f07e1da685585560b7387407c154d63
5h1vanshh <shivansh.paliwal@razorpay.com>
2025-03-03 12:03:57 +0530

    modified db.sql

--- internal/models/order.go
+++ internal/models/order.go
@@ -5,7 +5,7 @@ import "time"
 type Order struct {
 	ID         string    `gorm:"primaryKey" json:"id"`
 	CustomerID string    `json:"customer_id"`
-	ProductID  string    `json:"product_id"`
+	ProductID  int       `json:"product_id"`
 	Quantity   int       `json:"quantity"`
 	Status     string    `json:"status"`
 	CreatedAt  time.Time `json:"created_at"`
